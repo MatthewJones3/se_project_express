03651519a337d6690a0a57eb30b37dc3814ed1b3
Matthew <MattyJones1615@gmail.com>
2025-02-08 17:50:34 -0500

    Tutor purposes last 2 npx eslint fixes

--- controllers/users.js
+++ controllers/users.js
@@ -1,27 +1,29 @@
-/* const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 const {
   BAD_REQUEST,
-  NOT_FOUND,
+  UNAUTHORIZED,
   INTERNAL_SERVER_ERROR,
+  CONFLICT,
 } = require("../utils/errors");
 const { JWT_SECRET } = require("../utils/config");
 
 const loginUser = async (req, res) => {
   const { email, password } = req.body;
-
   try {
     const user = await User.findOne({ email }).select("+password");
     if (!user) {
-      return res.status(NOT_FOUND).json({ message: "Invalid email or password" });
+      return res
+        .status(UNAUTHORIZED)
+        .json({ message: "Invalid email or password" });
     }
-
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(NOT_FOUND).json({ message: "Invalid email or password" });
+      return res
+        .status(UNAUTHORIZED)
+        .json({ message: "Invalid email or password" });
     }
-
     const token = jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: "7d" });
     return res.status(200).json({ token });
   } catch (error) {
@@ -33,96 +35,12 @@ const loginUser = async (req, res) => {
 
 const createUser = async (req, res) => {
   const { name, avatar, email, password } = req.body;
-
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = new User({ name, avatar, email, password: hashedPassword });
     await user.save();
-    const token = jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: "7d" });
-    return res.status(201).json({ token });
-  } catch (error) {
-    if (error.code === 11000) {
-      return res.status(409).json({ message: "Email is already in use" });
-    }
-    if (error.name === "ValidationError") {
-      return res.status(BAD_REQUEST).json({ message: error.message });
-    }
-    return res
-      .status(INTERNAL_SERVER_ERROR)
-      .json({ message: "An error has occurred on the server." });
-  }
-};
-
-const getCurrentUser = async (req, res) => {
-  try {
-    const user = await User.findById(req.user._id);
-    return res.status(200).json(user);
-  } catch (error) {
-    return res
-      .status(INTERNAL_SERVER_ERROR)
-      .json({ message: "An error has occurred on the server." });
-  }
-};
-
-const updateUser = async (req, res) => {
-  const { name, avatar } = req.body;
-
-  try {
-    const user = await User.findByIdAndUpdate(
-      req.user._id,
-      { name, avatar },
-      { new: true, runValidators: true }
-    );
-    return res.status(200).json(user);
-  } catch (error) {
-    if (error.name === "CastError") {
-      return res.status(BAD_REQUEST).json({ message: "Invalid ID format" });
-    }
-    if (error.name === "ValidationError") {
-      return res.status(BAD_REQUEST).json({ message: error.message });
-    }
-    return res
-      .status(INTERNAL_SERVER_ERROR)
-      .json({ message: "An error has occurred on the server." });
-  }
-};
-
-module.exports = { loginUser, createUser, getCurrentUser, updateUser }; */
-
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
-const User = require("../models/user");
-const { BAD_REQUEST, UNAUTHORIZED, INTERNAL_SERVER_ERROR, CONFLICT } = require("../utils/errors");
-const { JWT_SECRET } = require("../utils/config");
-
-const loginUser = async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const user = await User.findOne({ email }).select("+password");
-    if (!user) {
-      return res.status(UNAUTHORIZED).json({ message: "Invalid email or password" });
-    }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(UNAUTHORIZED).json({ message: "Invalid email or password" });
-    }
-
-    const token = jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: "7d" });
-    return res.status(200).json({ token });
-  } catch (error) {
-    return res.status(INTERNAL_SERVER_ERROR).json({ message: "An error has occurred on the server." });
-  }
-};
-
-const createUser = async (req, res) => {
-  const { name, avatar, email, password } = req.body;
-  try {
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = new User({ name, avatar, email, password: hashedPassword });
-    await user.save();
-    const token = jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: "7d" });
-    return res.status(201).json({ token });
+    const { password: _, ...userData } = user.toObject();
+    return res.status(201).json(userData); // Returning user data without the password
   } catch (error) {
     if (error.code === 11000) {
       return res.status(CONFLICT).json({ message: "Email is already in use" });
@@ -130,16 +48,21 @@ const createUser = async (req, res) => {
     if (error.name === "ValidationError") {
       return res.status(BAD_REQUEST).json({ message: error.message });
     }
-    return res.status(INTERNAL_SERVER_ERROR).json({ message: "An error has occurred on the server." });
+    return res
+      .status(INTERNAL_SERVER_ERROR)
+      .json({ message: "An error has occurred on the server." });
   }
 };
 
 const getCurrentUser = async (req, res) => {
   try {
     const user = await User.findById(req.user._id);
-    return res.status(200).json(user);
+    const { password, ...userData } = user.toObject();
+    return res.status(200).json(userData); // Returning user data without password
   } catch (error) {
-    return res.status(INTERNAL_SERVER_ERROR).json({ message: "An error has occurred on the server." });
+    return res
+      .status(INTERNAL_SERVER_ERROR)
+      .json({ message: "An error has occurred on the server." });
   }
 };
 
@@ -151,7 +74,8 @@ const updateUser = async (req, res) => {
       { name, avatar },
       { new: true, runValidators: true }
     );
-    return res.status(200).json(user);
+    const { password, ...userData } = user.toObject();
+    return res.status(200).json(userData); // Returning updated user data without password
   } catch (error) {
     if (error.name === "CastError") {
       return res.status(BAD_REQUEST).json({ message: "Invalid ID format" });
@@ -159,10 +83,10 @@ const updateUser = async (req, res) => {
     if (error.name === "ValidationError") {
       return res.status(BAD_REQUEST).json({ message: error.message });
     }
-    return res.status(INTERNAL_SERVER_ERROR).json({ message: "An error has occurred on the server." });
+    return res
+      .status(INTERNAL_SERVER_ERROR)
+      .json({ message: "An error has occurred on the server." });
   }
 };
 
 module.exports = { loginUser, createUser, getCurrentUser, updateUser };
-
-
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,50 +1,23 @@
-/* const jwt = require("jsonwebtoken");
-const { JWT_SECRET } = require("../utils/config");
-const User = require("../models/user");
-
-const auth = async (req, res, next) => {
-  try {
-    const token = req.header("Authorization")?.replace("Bearer ", "");
-    if (!token) {
-      return res.status(401).json({ message: "Please authenticate." });
-    }
-
-    const decoded = jwt.verify(token, JWT_SECRET);
-    const user = await User.findById(decoded._id);
-    if (!user) {
-      return res.status(401).json({ message: "User not found" });
-    }
-
-    req.user = user;
-    return next();
-  } catch (error) {
-    return res.status(401).json({ message: "Authentication failed." });
-  }
-};
-
-module.exports = auth; */
-
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../utils/config");
 const User = require("../models/user");
+const { UNAUTHORIZED } = require("../utils/errors");
 
 const auth = async (req, res, next) => {
   try {
     const token = req.header("Authorization")?.replace("Bearer ", "");
     if (!token) {
-      return res.status(401).json({ message: "Please authenticate." });
+      return res.status(UNAUTHORIZED).json({ message: "Please authenticate." });
     }
-
     const decoded = jwt.verify(token, JWT_SECRET);
     const user = await User.findById(decoded._id);
     if (!user) {
-      return res.status(401).json({ message: "User not found" });
+      return res.status(UNAUTHORIZED).json({ message: "User not found" });
     }
-
     req.user = user;
     return next();
   } catch (error) {
-    return res.status(401).json({ message: "Authentication failed." });
+    return res.status(UNAUTHORIZED).json({ message: "Authentication failed." });
   }
 };
 
